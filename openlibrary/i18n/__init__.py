17495d54ba3d9b32747bac16129309f283fc88c0
James Champ <jameschamp@acm.org>
2021-07-13 15:29:07 -0400

    Fix linting errors

--- openlibrary/i18n/__init__.py
+++ openlibrary/i18n/__init__.py
@@ -90,6 +90,7 @@ def compile_translations():
         if os.path.exists(po_path):
             _compile_translation(po_path, mo_path)
 
+
 def update_translations(locales=[]):
     if locales:
         print(f"Updating the following locales: {locales}")
@@ -97,7 +98,7 @@ def update_translations(locales=[]):
     else:
         print("Updating all locales")
         locales_to_update = get_locales()
-        
+
     pot_path = os.path.join(root, 'messages.pot')
     template = read_po(open(pot_path, 'rb'))
 
